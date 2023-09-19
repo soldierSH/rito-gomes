777221807edf80b7f2dc67a89052e4207f507ef7
soldierSH <scarlethanna29@gmail.com>
2023-09-19 20:42:25 -0300

    mudança de pastas

--- back/champions/src/main/java/com/soldier/champions/ChampionController.java
+++ back/champions/src/main/java/com/soldier/champions/ChampionController.java
@@ -5,6 +5,7 @@ import com.soldier.champions.model.Campeao;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
